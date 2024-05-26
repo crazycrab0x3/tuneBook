fe50236de760e2a92b0e630d5b23a51290b8958a
crazycrab0x3 <ichirohasegawa0x3@gmail.com>
2024-05-26 11:08:30 +0900

    use element-ui

--- src/tuneBook_frontend/vite.config.ts
+++ src/tuneBook_frontend/vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
 import vue from '@vitejs/plugin-vue';
 import dotenv from 'dotenv';
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite'
 
 dotenv.config({ path: '../../.env' });
 
@@ -28,7 +32,16 @@ export default defineConfig({
   plugins: [
     vue(),
     environment('all', { prefix: 'CANISTER_' }),
-    environment('all', { prefix: 'DFX_' }),
+    environment('all', { prefix: 'DFX_' }),  
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    ElementPlus({
+      // options
+    })
   ],
   resolve: {
     alias: [
