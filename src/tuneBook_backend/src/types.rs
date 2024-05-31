2f4ece2128aa69825ed6ec951c6197d7da386305
crazycrab0x3 <ichirohasegawa0x3@gmail.com>
2024-06-01 03:18:42 +0900

    update

--- src/tuneBook_backend/src/types.rs
+++ src/tuneBook_backend/src/types.rs
@@ -1,4 +1,4 @@
-use candid::{CandidType, Deserialize};
+use candid::{ Principal, CandidType, Deserialize};
 
 #[derive(CandidType, Clone, Debug)]
 pub struct Profile {
@@ -6,10 +6,40 @@ pub struct Profile {
     pub username: String,
     pub avatar: Vec<u8>
 }
+
 #[derive(CandidType, Clone, Deserialize, Debug)]
 pub struct Tune {
     pub origin: bool,
     pub title: String,
     pub tune_data: Option<String>,
     pub timestamp: String
+}
+
+#[derive(CandidType, Clone, Deserialize, Debug)]
+pub struct OnSetDocContext {
+    pub caller: Principal,
+    pub data: DocContext
+}
+
+#[derive(CandidType, Clone, Deserialize, Debug)]
+pub struct DocContext {
+    pub collection: String,
+    pub key: String,
+    pub data: DocUpsert
+}
+
+#[derive(CandidType, Clone, Deserialize, Debug)]
+pub struct DocUpsert {
+    pub before: Option<Doc>,
+    pub after: Doc
+}
+
+#[derive(CandidType, Clone, Deserialize, Debug)]
+pub struct Doc {
+    pub owner: Principal,
+    pub data: Vec<u8>,
+    pub description: Option<String>,
+    pub created_at: u64,
+    pub updated_at: u64,
+    pub version: Option<u64>,
 }
